8d6cf243a6793fd8faec079555685af3a882476f
Sumaiya Rahman Chowdhury <sumaiyaprionty@gmail.com>
2026-08-19 05:20:16 +0600

    slicing

---
+++ slicing.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+#print(arr[1:5]) 
+
+#start at 1 and stop at 5 (not included)
+
+#print(arr[4:])
+#start at 4 and go to end of the array
+
+#slice from beginning
+#print(arr[:4])
+
+#Slicing 2-D Arrays
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+#print(arr[1, 1:4]) 
+print(arr[0:1,2])
